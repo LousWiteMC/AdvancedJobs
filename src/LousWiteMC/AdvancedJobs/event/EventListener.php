07820be1ccf27ccf51fea62e7d96ab78ea3e5b3d
LousWiteMC <37389095+LousWiteMC@users.noreply.github.com>
2020-04-10 18:49:33 +0700

    Add files via upload

---
+++ src/LousWiteMC/AdvancedJobs/event/EventListener.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace LousWiteMC\AdvancedJobs\event;
+
+use pocketmine\event\Listener;
+use pocketmine\event\PlayerJoinEvent;
+use pocketmine\event\block\BlockBreakEvent;
+use pocketmine\event\block\BlockPlaceEvent;
+use pocketmine\event\entity\EntityDamageByEntityEvent;
+use pocketmine\Player;
+use LousWiteMC\AdvancedJobs\AdvancedJobs;
+use pocketmine\block\{Wood, Wood2, DiamondOre, Diamond, Iron, IronOre, Gold, GoldOre, Emerald, EmeraldOre, Stone, Cobblestone, Redstone, RedstoneOre, Coal, CoalOre, Lapis, LapisOre};
+use pocketmine\entity\{Animal, Monster};
+
+class EventListener implements Listener{
+
+	public function __construct(AdvancedJobs $plugin){
+		$this->plugin = $plugin;
+	}
+
+	public function onBreak(BlockBreakEvent $event){
+		$player = $event->getPlayer();
+		$block = $event->getBlock();
+		if($block instanceof Wood or $block instanceof Wood2){
+			if($this->plugin->hasJob($player)){
+				if($this->plugin->getJob($player) == "wood-cutter"){
+					$this->plugin->addProgress($player);
+				}
+			}
+		}elseif($block instanceof Diamond or $block instanceof DiamondOre or $block instanceof Iron or $block instanceof IronOre or $block instanceof Gold or $block instanceof GoldOre or $block instanceof Emerald or $block instanceof EmeraldOre or $block instanceof Stone or $block instanceof Cobblestone or $block instanceof Redstone or $block instanceof RedstoneOre or $block instanceof Coal or $block instanceof CoalOre or $block instanceof Lapis or $block instanceof LapisOre){
+			if($this->plugin->hasJob($player)){
+				if($this->plugin->getJob($player) == "miner"){
+					$this->plugin->addProgress($player);
+				}
+			}
+		}
+	}
+
+	public function onPlace(BlockPlaceEvent $event){
+		$player = $event->getPlayer();
+		if($this->plugin->hasJob($player)){
+			if($this->plugin->getJob($player) == "builder"){
+				$this->plugin->addProgress($player);
+			}
+		}
+	}
+
+	public function onKill(EntityDamageByEntityEvent $event){
+		$player = $event->getDamager();
+		$entity = $event->getEntity();
+		if($player instanceof Player){
+			if($this->plugin->hasJob($player)){
+				if($entity instanceof Player){
+					if($this->plugin->getJob($player) == "killer"){
+						$this->plugin->addProgress($player);
+					}
+				}
+			}
+		}
+	}
+}
